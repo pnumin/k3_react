9a3addb2946ffc53617eb4e0e9d8f8755adf644e
pnumin <pnumin@pusan.ac.kr>
2023-05-04 12:08:18 +0900

    useRef

--- src/07/MyRef.js
+++ src/07/MyRef.js
@@ -1,23 +1,34 @@
+import { useState } from "react";
 const MyRef = () => {
     let cnt1 = 1;
+    const [cnt2, setCnt2] = useState(1) ;
 
     const showCnt = () => {
-        console.log('cnt1 =', cnt1);
+        console.log('cnt1 =', cnt1, 'cnt2 =', cnt2);
     }
 
     const showCnt1 = () => {
+        cnt1 = cnt1 + 1 ;
         showCnt();
     }
+
+    const showCnt2 = () => {
+        setCnt2(cnt2 + 1) ;
+        showCnt();
+    }
+
     return (
         <main className="container">
             <article>
                 <header>
                     <div className="grid">
-                        <div>컴포넌트 변수 : {cnt1}</div>
+                        <div><h1>컴포넌트 변수 : {cnt1}</h1></div>
+                        <div><h1>state 변수 : {cnt2}</h1></div>
                     </div>
                 </header>
                 <div className="grid">
                     <button onClick={() => showCnt1()}>컴포넌트 변수</button>
+                    <button onClick={() => showCnt2()}>state 변수</button>
                 </div>
             </article>
         </main>
